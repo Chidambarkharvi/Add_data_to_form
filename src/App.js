99208ad9d7125273ac1d450fbfdbad9d010715a2
Chidambarkharvi <chidambarkharvi5@gmail.com>
2022-01-18 17:46:22 +0530

    registration data to form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            backgroundColor: "rgb(56, 102, 189)",
+            
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <p
+              style={{
+               
+              }}
+            >
+              <i style={{fontSize:"40px" , color:"white"  ,marginLeft:"50px", marginTop:"10px"}} class="fab fa-pied-piper-pp"></i>
+            </p>
+          </div>
+          <nav
+            style={{ 
+
+              width: "30%",
+              paddingTop: "20px",
+              display: "flex",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Link style={{ color: "white", textDecoration: "none", marginRight:"100px" }} to="/home">
+              Home
+            </Link>
+            <Link style={{ color: "white", textDecoration: "none" ,marginRight:"100px"  }} to="/login">
+              Login
+            </Link>
+            <Link
+              style={{ color: "white", textDecoration: "none" ,marginRight:"90px" }}
+              to="/register"
+            >
+              Register
+            </Link>
+          </nav>
+        </div>
+
+
+        <div>
+          <Route
+            path="/home"
+            render={(props) => {
+              return <Home userName="sachin" />;
+            }}
+          />
+          
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
